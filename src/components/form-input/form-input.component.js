7d667acf5944ee670c443292df4481427556f43f
Pratheeksh <pratheeksh@qburst.com>
2020-07-28 16:36:48 +0530

    Finishing sign-in and handleSubmit method.

--- src/components/form-input/form-input.component.js
+++ src/components/form-input/form-input.component.js
@@ -4,7 +4,7 @@ import './form-input.styles.scss';
 const FormInput = ({ handleChange, handleBlur, label, error, ...otherProps }) => {
     return (
         <div className={`group ${error ? 'error' : ''}`}>
-            <input className="form-input" onChange={handleChange} {...otherProps} onBlur={handleBlur} />
+            <input className="form-input" onChange={handleChange} {...otherProps} onBlur={handleBlur} required="required"/>
             {
                 label ? (
                     <div className={`form-input-label ${otherProps.value.length > 0 ? 'shrink' : ''}`}>
--- src/components/sign-in/sign-in.component.js
+++ src/components/sign-in/sign-in.component.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './sign-in.styles.scss';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { signInWithGoogle, auth } from '../../firebase/firebase.utils';
 
 class SignIn extends React.Component {
     constructor(props) {
@@ -13,9 +13,17 @@ class SignIn extends React.Component {
         };
     }
 
-    handleSubmit = event => {
+    handleSubmit = async event => {
         event.preventDefault();
-        this.setState({ email: '', password: '' });
+        const { email, password } = this.state;
+        if (email && password) {
+            try {
+                await auth.signInWithEmailAndPassword(email, password);
+                this.setState({ email: '', password: '' });
+            } catch (error) {
+                console.error(`Login Error ::> ${error}`);
+            }
+        }
     }
 
     handleChange = event => {
